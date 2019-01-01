621a4c6a29b5c3afecd3349fd466303cc74f7a04
Simone Conti <s.conti@itnok.com>
2019-01-01 00:14:58 -0800

    [IMPROVEMENT] Better script to install Docker on the host system

--- install_docker.sh
+++ install_docker.sh
@@ -1,6 +1,23 @@
-#!/bin/bash
-#Docker install for a debian based system
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce
+#!/usr/bin/env bash
+
+# Docker install for a debian based system
+sudo /usr/bin/env bash -c ' \
+    apt-get update -y && \
+    apt-get install -y \
+        apt-transport-https \
+        ca-certificates \
+        curl \
+        software-properties-common && \
+    curl -fsSL \
+        https://download.docker.com/linux/ubuntu/gpg | \
+        apt-key add - && \
+    add-apt-repository \
+        "deb [arch=$(dpkg-architecture | grep "DEB_TARGET_ARCH=" | cut -d= -f2)] \
+        https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) \
+        stable" && \
+    apt-get update -y && \
+    apt-get install -y \
+        docker-ce && \
+    docker --version \
+';
