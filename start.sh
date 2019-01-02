56899cba82b919f0432473bcf5881b58d029371f
Simone Conti <s.conti@itnok.com>
2019-01-01 23:28:58 -0800

    [BUG] HTTPS section in start.sh was never executed

--- start.sh
+++ start.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
-# start supervisord to bring up webserver and sleep to be sure it's up
-supervisord -n
-
 # Run letsencrypt, if the parameters were given. Output a log of the letsencrypt attempt
-if [ -n "$1" ]; then
-    python /home/flask/conf/setup-https.py $1 > /home/flask/letsencrypt-setup.txt
+if [ -n "${1}" ]; then
+    service nginx restart
+    python /home/flask/conf/setup-https.py ${1} | tee /home/flask/letsencrypt-setup.txt
+    service nginx stop
 fi
+
+# start supervisord to bring up webserver and sleep to be sure it's up
+supervisord -c /etc/supervisor/conf.d/supervisor.conf
+
